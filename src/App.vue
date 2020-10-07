15b1729c4eef2e23763ef96c29787f227083ceac
LoginALee <Alejandrodxwwe@hotmail.com>
2020-10-07 12:20:29 -0500

    Adding userProfile component

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <template>
   <div id="app">
-   @ {{  user.username }} - {{ fullName }}
-   <strong>Followers:</strong> {{ followers }}
-   <button v-on:click='followUser'>Follow</button>
+   <UserProfile/>
   </div>
   
 </template>
 
 <script>
-
+import UserProfile from './components/UserProfile';
 
 export default {
   name: 'App',
-  data(){
-    return{
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'LoginALee',
-        firstName: 'Alejandro',
-        lastName: 'Tinoco',
-        email: 'admin123@gmail.com',
-        isAdmin: true
-      }
-    }
-  },
-
-  watch: {
-    followers(newFollowerCount, oldFollowerCount){
-      if(oldFollowerCount < newFollowerCount){
-        console.log(`${this.user.username} has gained a follower!`)
-      }
-    }
-  },
-
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-    
-  },
-
-  methods: {
-    followUser(){
-      this.followers++;
-    }
-  },
-
-  mounted(){
-    this.followUser();
-  }
+  components: { UserProfile }
 }
 </script>
 
@@ -59,8 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
+  min-height: 100vh;
+  background-color: #F3F5FA;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{ user.username }}</h1>
+            <div class="user-profile__follower-count">
+                <strong>Followers: </strong> {{ followers }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  data(){
+    return{
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'LoginALee',
+        firstName: 'Alejandro',
+        lastName: 'Tinoco',
+        email: 'admin123@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+    
+  },
+
+  methods: {
+    followUser(){
+      this.followers++;
+    }
+  },
+
+  mounted(){
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+.user-profile{
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile__user-panel{
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+</style>
